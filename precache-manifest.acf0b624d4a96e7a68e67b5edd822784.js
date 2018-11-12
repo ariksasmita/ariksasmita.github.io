5e88c2bc5c36da800fbaf430345542e692e341e2
Arik Sasmita <arik.sasmita@akqa.com>
2018-11-12 10:50:14 +0800

    dana.id demo site deployment

---
+++ precache-manifest.acf0b624d4a96e7a68e67b5edd822784.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "10d14b8f9cda627d8ebf324348888f9d",
+    "url": "/static/media/arrow-left.10d14b8f.svg"
+  },
+  {
+    "revision": "dd978c92ef440a7c460c",
+    "url": "/static/css/main.13a64754.chunk.css"
+  },
+  {
+    "revision": "2861f5ba7cecdd5064c5",
+    "url": "/static/js/1.2861f5ba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b392c2f90e83df8489a0c39e1de2f3b0",
+    "url": "/static/media/support.b392c2f9.svg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/static/media/loading.be00fc4a.svg"
+  },
+  {
+    "revision": "dd978c92ef440a7c460c",
+    "url": "/static/js/main.dd978c92.chunk.js"
+  },
+  {
+    "revision": "86a34ae645ab3046e325f1bd61737627",
+    "url": "/static/media/rainy@3x.86a34ae6.jpg"
+  },
+  {
+    "revision": "78314612551c56dc36f40ebe72dc522e",
+    "url": "/static/media/navigate.78314612.svg"
+  },
+  {
+    "revision": "a347ea9777c0509cebe59757c7ffe648",
+    "url": "/static/media/logo-dana-with-typography.a347ea97.svg"
+  },
+  {
+    "revision": "cfafd01ed60e43864167a2cd0441bf0a",
+    "url": "/static/media/black.cfafd01e.svg"
+  },
+  {
+    "revision": "e76abb4b453be13750d9c5dc21365a9a",
+    "url": "/static/media/blue.e76abb4b.svg"
+  },
+  {
+    "revision": "1fef41854fa0887b993f683420f41189",
+    "url": "/index.html"
+  }
+];
